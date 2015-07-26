d7d65530a4f10b3337b5446c3142c22be4b2d215
Jesus-Vila <vila@ciencias.unam.mx>
2015-07-26 16:42:20 +0000

    Falta cerrar menú mobil

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -1,5 +1,28 @@
 var ready = function() {
+	$('#menu-icon').on('click', function () {
+		$('#logo-mobile').hide();
+		$('#menu-icon').hide();
 
+		$('#menu-mobile').animate({
+			left: "0px"
+		}, 200);
+
+		$('body').animate({
+			left: "78.125%"
+		}, 200);
+	});
+/*
+	$('#menu-icon').on('click', function () {
+		$('#logo-mobile').show();
+
+		$('#menu-mobile').animate({
+			left: "78.125%"
+		}, 200);
+
+		$('body').animate({
+			left: "0px"
+		}, 200);
+	});*/
 }
 
 
